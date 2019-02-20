7de2f90e52aff63dac0799af8ea7ca9dcffcf71f
FRC6553 <frcrobotics6553@gmail.com>
2019-02-19 19:31:16 -0800

    Havn't commited in a while. Removed Pneumatics, added Actuator code. untesed.

--- FRC 2019 Robot Code/src/main/java/frc/robot/Robot.java
+++ FRC 2019 Robot Code/src/main/java/frc/robot/Robot.java
@@ -19,39 +19,63 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.CameraServer;
 
 
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
-  public static Spark leftWheel;
-  public static Spark rightWheel;
-  public static Spark rightReverseWheel;
-  public static Spark leftReverseWheel;
+  public static Spark grabberLeftWheel;
+  public static Spark grabberRightWheel;
+  public static Spark GrabberRightReverseWheel;
+  public static Spark GrabberLeftReverseWheel;
   public static Joystick driveStick;
   public static DifferentialDrive myDrive;
-  public static Spark leftMotors;
-  public static Spark rightMotors;
+  public static Spark grabberLeftMotors;
+  public static Spark grabberRightMotors;
   public static SpeedControllerGroup leftDrive;
   public static SpeedControllerGroup rightDrive;
   public static Spark elevatorMotor;
-  //pneumatics
+  //actuators
   public static XboxController xbox;
-  public static DoubleSolenoid front;
-  public static DoubleSolenoid rear;
-  public static Compressor air;
+  public static Spark fAct;
+  //fAct = Front Actuator
+  public static Spark rAct;
+  //rAct = Rear Actuator
+
+  // public static DoubleSolenoid rear;
+  // public static DoubleSolenoid front;
+  // public static Compressor air;
 //!!controllVariables for grabbers
   public static int leftGrabberDirection = 1;
   public static int rightGrabberDirection = 1;
   public static double leftGrabberSpeed = 0.4;
   public static double rightGrabberSpeed = 0.4;
   public double grabberSpeedRatio = 0;
-  public double grabberSpeedStep = 0.01;
+  public double grabberSpeedStep = 0.4;
 //!!ControllVariables for Elevator
-public static double elevatorMotorSpeed = 1;
+public static double elevatorMotorSpeed = 1.0;
 public static int elevatorMotorDirection = 1;
 public double elevatorMotorSpeedRatio = 0;
-public double elevatorMotorSpeedStep = 0.05;
+public double elevatorMotorSpeedStep = 0.1;
+//!!controllVariables for Platform
+public double platformMotorSpeed = 1.0;
+public double driveMotorSpeed = 1.0;
+//!!controll Variables for Actuators
+public static double frontActuator = 1.0;
+public static int frontActuatorDirection = 1;
+public static double rearActuator = 1.0;
+public static int rearActuatorDirection = 1;
+
+//PWM spots
+public int rightMotorsPWM = 0;
+public int leftMotorsPWM = 1;
+public int leftWheelPWM = 2;
+public int elevatorMotorPWM = 5;
+
+//Xbox buttons
+public int leftBumper = 5;
+public int rightBumper = 6;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -60,26 +84,31 @@ public double elevatorMotorSpeedStep = 0.05;
   @Override
   public void robotInit() {
     //robot motor controlls
-    leftWheel = new Spark(2);
-    rightWheel = new Spark(3);
+    grabberLeftWheel = new Spark(2);
+    grabberRightWheel = new Spark(3);
     //rightReverseWheel = new Spark(2);
     //leftReverseWheel = new Spark(1);
     driveStick = new Joystick(0);
-    leftMotors=new Spark(1);
-		leftDrive =new SpeedControllerGroup(leftMotors);
-		rightMotors=new Spark(0);
-    rightDrive=new SpeedControllerGroup(rightMotors);
-    myDrive = new DifferentialDrive(leftMotors, rightMotors);
+    grabberLeftMotors=new Spark(1);
+		leftDrive =new SpeedControllerGroup(grabberLeftMotors);
+		grabberRightMotors=new Spark(0);
+    rightDrive=new SpeedControllerGroup(grabberRightMotors);
+    myDrive = new DifferentialDrive(grabberLeftMotors, grabberRightMotors);
     //robot pneumatic controlls
     xbox = new XboxController(1);
-    front = new DoubleSolenoid(1,0);
-    air = new Compressor(0);
-    air.setClosedLoopControl(false);
-    elevatorMotor = new Spark(5);
+    fAct = new Spark(7);
+    rAct = new Spark(6);
+
+    // rear = new DoubleSolenoid(1,0);
+    // front = new DoubleSolenoid(2,3);
+    // air = new Compressor(0);
+    // air.setClosedLoopControl(false);
+    elevatorMotor = new Spark(elevatorMotorPWM);
    // m_oi = new OI();
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+    CameraServer.getInstance().startAutomaticCapture();
   }
 
   @Override
@@ -123,65 +152,121 @@ public double elevatorMotorSpeedStep = 0.05;
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
   //Joystick
-    myDrive.arcadeDrive(-driveStick.getY(),driveStick.getX());
+    myDrive.arcadeDrive(driveStick.getY() * driveMotorSpeed,driveStick.getX() * driveMotorSpeed);
     
   //Trigger & Side Button
+  //intake cargo
     if (driveStick.getRawButton(1)&&!driveStick.getRawButton(2)){
       grabberSpeedRatio = grabberSpeedRatio + grabberSpeedStep; 
       if (grabberSpeedRatio >= 1.0){
         grabberSpeedRatio = 1;
       }
-    leftWheel.setSpeed(leftGrabberDirection * leftGrabberSpeed * grabberSpeedRatio);
-    rightWheel.setSpeed(rightGrabberDirection * rightGrabberSpeed * grabberSpeedRatio);
+    grabberLeftWheel.setSpeed(leftGrabberDirection * leftGrabberSpeed * grabberSpeedRatio);
+    grabberRightWheel.setSpeed(rightGrabberDirection * rightGrabberSpeed * grabberSpeedRatio);
+//shoot out cargo
   }else if (driveStick.getRawButton(2)&&!driveStick.getRawButton(1)){
-    leftWheel.setSpeed(-1*leftGrabberDirection * leftGrabberSpeed);
-    rightWheel.setSpeed(-1*rightGrabberDirection * rightGrabberSpeed);
+    grabberLeftWheel.setSpeed(-1 * leftGrabberDirection * leftGrabberSpeed);
+    grabberRightWheel.setSpeed(-1 * rightGrabberDirection * rightGrabberSpeed);
   }else{
     grabberSpeedRatio = 0;
-   leftWheel.stopMotor();
-   rightWheel.stopMotor();
+   grabberLeftWheel.stopMotor();
+   grabberRightWheel.stopMotor();
   }
 //Xbox controller (pneumatics)
 //Start Button
-if(xbox.getStartButton()&&!xbox.getBackButton()) {
-  air.setClosedLoopControl(true);
-}
-//Back Button
-if(xbox.getBackButton()) {
-	air.setClosedLoopControl(false);
-}
-//X Button
-if(xbox.getXButton()){
-  front.set(DoubleSolenoid.Value.kForward);				
-}
-//Y Button
-else if(xbox.getYButton()){
-  front.set(DoubleSolenoid.Value.kReverse);
-}
-//off
-else {
-  front.set(DoubleSolenoid.Value.kOff);
-}
-// //A Button
-// if(xbox.getAButton()){
-//   rear.set(DoubleSolenoid.Value.kForward);				
+
+
+
+// if(xbox.getStartButton()&&!xbox.getBackButton()) {
+//   air.setClosedLoopControl(true);
 // }
-// //B Button
-// else if(xbox.getBButton()){
+
+//Back Button
+
+
+// if(xbox.getBackButton()) {
+// 	air.setClosedLoopControl(false);
+// }
+
+//X Button
+//Front Actuator In
+
+if(xbox.getXButton()){
+  fAct.setSpeed(-1 * frontActuatorDirection * frontActuator);
+}
+
+// if(xbox.getXButton()){
+//   rear.set(DoubleSolenoid.Value.kForward);	
+//   driveMotorSpeed = 0.5;
+// }
+
+//Y Button
+//Front Actuator Out
+
+else if(xbox.getYButton()){
+  fAct.setSpeed(frontActuatorDirection * frontActuator);
+}
+
+else{
+  fAct.stopMotor();
+}
+
+// else if(xbox.getYButton()){
 //   rear.set(DoubleSolenoid.Value.kReverse);
 // }
-// //off
+
+//off
+
+
+
 // else {
 //   rear.set(DoubleSolenoid.Value.kOff);
+//   driveMotorSpeed = 1.0;
 // }
-//xbox axis
-if(xbox.getRawButton(5)){
+
+//A Button
+//Rear Actuator In
+if(xbox.getAButton()){
+  rAct.setSpeed(-1 * frontActuatorDirection * frontActuator);
+}
+
+// if(xbox.getAButton()){
+//   front.set(DoubleSolenoid.Value.kForward);
+//   platformMotorSpeed = 0.5;				
+// }
+
+//B Button
+//Rear Actuator Out
+else if(xbox.getBButton()){
+  rAct.setSpeed(frontActuatorDirection * frontActuator);
+}
+
+else{
+  rAct.stopMotor();
+}
+
+
+// else if(xbox.getBButton()){
+//   front.set(DoubleSolenoid.Value.kReverse);
+// }
+
+//off
+
+
+
+// else {
+//   front.set(DoubleSolenoid.Value.kOff);
+//   platformMotorSpeed = 1.0;
+// }
+
+//elevator up (Left and Right Bumpers)
+if(xbox.getRawButton(leftBumper)){
   elevatorMotorSpeedRatio = elevatorMotorSpeedRatio + elevatorMotorSpeedStep; 
   if (elevatorMotorSpeedRatio >= 1.0){
     elevatorMotorSpeedRatio = 1;
   }
   elevatorMotor.setSpeed(elevatorMotorDirection * elevatorMotorSpeed * elevatorMotorSpeedRatio);
-}else if(xbox.getRawButton(6)){
+}else if(xbox.getRawButton(rightBumper)){
   elevatorMotorSpeedRatio = elevatorMotorSpeedRatio + elevatorMotorSpeedStep; 
   if (elevatorMotorSpeedRatio >= 1.0){
     elevatorMotorSpeedRatio = 1;
@@ -189,13 +274,11 @@ if(xbox.getRawButton(5)){
   elevatorMotor.setSpeed(-elevatorMotorDirection * elevatorMotorSpeed * elevatorMotorSpeedRatio);
 }else{
   elevatorMotor.stopMotor();
-
 }
+
 }
 
   @Override
   public void testPeriodic() {
   }
 }
-
-//
